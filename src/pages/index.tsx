3525a287366738e7b0d3f012b3fe115465bbfa7b
Saumay <ksaumya1510@gmail.com>
2024-04-19 15:25:34 -0400

    updated api key

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,38 +2,92 @@ import SearchBar from "@/components/SearchBar";
 import Weather from "@/components/Weather";
 import axios from "axios";
 import { Poppins } from "next/font/google";
-import { useEffect, useState } from "react";
+import {
+  useEffect,
+  useState,
+} from "react";
 
-const inter = Poppins({ subsets: ["latin"], weight: "500" });
-export default function Home() {
-  const [search, setSearch] = useState<string>("");
-  const [weather, setWeather] = useState([]);
-  const searchWeatherHandler = () => {
-    if (search !== "") {
-      axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=3265874a2c77ae4a04bb96236a642d2f&units=metric`
-        )
-        .then((response) => {
-          setWeather(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+const inter =
+  Poppins(
+    {
+      subsets:
+        [
+          "latin",
+        ],
+      weight:
+        "500",
     }
-  };
+  );
+export default function Home() {
+  const [
+    search,
+    setSearch,
+  ] =
+    useState<string>(
+      ""
+    );
+  const [
+    weather,
+    setWeather,
+  ] =
+    useState(
+      []
+    );
+  const searchWeatherHandler =
+    () => {
+      if (
+        search !==
+        ""
+      ) {
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=398e35c54ecd2a0f30d3f613c2211878&units=metric`
+          )
+          .then(
+            (
+              response
+            ) => {
+              setWeather(
+                response.data
+              );
+            }
+          )
+          .catch(
+            (
+              error
+            ) => {
+              console.log(
+                error
+              );
+            }
+          );
+      }
+    };
   useEffect(() => {
     searchWeatherHandler();
-  }, [search]);
+  }, [
+    search,
+  ]);
   return (
     <div
       className={`w-screen flex flex-col h-screen max-w-[1240px] mx-auto mt-10 p-10"  ${inter.className}`}
     >
       <div>
-        <SearchBar selected={search} setSelected={setSearch} />
+        <SearchBar
+          selected={
+            search
+          }
+          setSelected={
+            setSearch
+          }
+        />
       </div>
       <div className="flex-1 flex items-center ">
-        <Weather weatherData={weather} />
+        <Weather
+          weatherData={
+            weather
+          }
+        />
       </div>
     </div>
   );
